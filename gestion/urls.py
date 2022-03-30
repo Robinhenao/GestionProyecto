6b41a52efe482d5f5fc7ca34b7c61c5e7af7e4d6
Robin Henao <robinson.henao@udea.edu.co>
2022-03-30 00:02:11 -0500

    add navigation, fix login requiered

--- gestion/urls.py
+++ gestion/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-from .views import gestion
+from .views import gestion,manage_students,make_student,make_project,admin_project
 
 urlpatterns = [
    path('', gestion, name="gestion_proyecto"),
+   path('manage_project/manage_students', manage_students, name="manage_students"),
+   path('manage_project/make_student', make_student, name="make_student"),
+   path('manage_project/admin_project', admin_project, name="admin_project"),
+   path('manage_project/make_project', make_project, name="make_project"),
    
 ]
--- gestion/views.py
+++ gestion/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='login')
 def gestion(request):
-    
     return render(request, "gestion_proyecto.html")
+
+def manage_students(request):
+    return render(request, "manage_students.html")
+
+def make_student(request):
+    return render(request, "make_student.html")
+
+def admin_project(request):
+    return render(request, "admin_project.html")
+
+def make_project(request):
+    return render(request, "make_projet.html")
+
