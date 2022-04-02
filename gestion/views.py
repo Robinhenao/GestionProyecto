c3475f540f1956f134150b30a78bdc85cca664d6
Robin Henao <robinson.henao@udea.edu.co>
2022-04-01 21:25:42 -0500

    addd paginator  an  content  page gestionprojectos

--- gestion/views.py
+++ gestion/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from gestion.forms import FormStudent
+from gestion.models import Proyecto
+from django.core.paginator import Paginator
 from django.contrib import messages
 
 
 @login_required(login_url='login')
 def gestion(request):
-    return render(request, "gestion_proyecto.html")
+    listado_posts =Proyecto.objects.all()
+    paginator = Paginator(listado_posts, 3)
+    pagina = request.GET.get("page") or 1
+    posts = paginator.get_page(pagina)
+    pagina_actual = int(pagina)
+    paginas = range(1, posts.paginator.num_pages + 1)
+    return render(request, "gestion_proyecto.html",{"projects":posts,"paginas": paginas, "pagina_actual": pagina_actual })
 
 def manage_students(request):
     return render(request, "manage_students.html")
